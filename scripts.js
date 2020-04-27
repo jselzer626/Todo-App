8f4b8d63fcb3cec583d9543e1547e765a2d9d4a5
Jon Selzer <jselzer626@gmail.com>
2020-04-27 14:41:24 -0400

    items added in tabbed list now show up in block/column

--- scripts.js
+++ scripts.js
@@ -1,34 +1,53 @@
 document.addEventListener("DOMContentLoaded", () => {
+
   //variable definitions
   let taskAddSpace = document.querySelector(".input-group")
   let formatToggle = document.querySelector("#formatToggle")
-  let tabDisplay = document.querySelector("#tabDisplay")
+  let tabDisplay = document.querySelector(".tabDisplay")
+  let blockColumnDisplay = document.querySelector("#blockColumnDisplay")
   let activities = {"notStarted": [], "inProgress": [], "completed": []}
+  const subLists = Object.keys(activities)
+
+  //create a list item
+  let createTask = (taskContent, taskContainer) => {
+    var newTask = document.createElement('li')
+    newTask.innerHTML = taskContent
+    taskContainer.appendChild(newTask)
+  }
 
   //configure buttons
   tabDisplay.querySelectorAll('p').forEach(tab => {
     tab.addEventListener('click', e => {
-      const listToRetrieve = e.target.id
+      //clear current selection
+      tabDisplay.querySelector("#selectedTab") ? tabDisplay.querySelector("#selectedTab").id = '' : ''
+      tab.id = "selectedTab"
+      const listToRetrieve = e.target.dataset.tab
       tabDisplay.querySelector('ul').innerHTML = ''
-      activities[listToRetrieve].forEach(item => {
-        var newTask = document.createElement('li')
-        newTask.innerHTML = item
-        tabDisplay.querySelector('ul').appendChild(newTask)
-      })
+      //load any existing activities
+      activities[listToRetrieve] ? activities[listToRetrieve].forEach(item => { createTask(item, tabDisplay.querySelector('ul')) }) : ''
     })
   })
-
 
   formatToggle.querySelectorAll('input').forEach(button => {
     button.addEventListener("click", e => {
       var currentView = document.querySelector(".selectedFormat")
+      //if selecting a different view than current
       if (`${e.target.id}display` != currentView.id) {
         currentView.classList.remove('selectedFormat')
         document.querySelector(`#${e.target.id}display`).classList.add('selectedFormat')
-        if (document.querySelector(".selectedFormat").className.includes('tab')) {
-          console.log('here')
-          tabDisplay.querySelector("#notStarted").click()
+
+        //select not started tab for tab view layout
+        if (document.querySelector(".selectedFormat").className.includes('tab'))
+          tabDisplay.querySelector(".notStarted").click()
+        else {
+          //load lists for block or column display
+          subLists.forEach(list => {
+            var container = blockColumnDisplay.querySelector(`.${list}`)
+            activities[list] ? activities[list].forEach(task => {createTask(task, container.querySelector('ul'))}) : ''
+          })
         }
+
+        //load messages for block and column display
       }
     })
   })
@@ -41,13 +60,8 @@ document.addEventListener("DOMContentLoaded", () => {
       window.alert("please enter something into the field")
       return false
     } else {
-      var container = document.querySelector(".selectedFormat ul")
-      var taskListItem = document.createElement('li')
-
       activities.notStarted.push(taskToAdd)
-      taskListItem.innerHTML = taskToAdd
-      container.appendChild(taskListItem)
-
+      createTask(taskToAdd, document.querySelector(".selectedFormat ul"))
     }
   }
 
